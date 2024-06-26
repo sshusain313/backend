5244c48c7ed40b6727f1c58f264de9bcc7bfd2ae
sshusain313 <113247030+sshusain313@users.noreply.github.com>
2024-06-27 00:35:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express=require('express');
+const app=express();
+const port=3000;
+
+app.get('/',(req,res)=>{
+    res.send('hello world');
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('shabahathusain')
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>please login at chai aur code</h1>')
+})
+
+app.get('/youtube', (req,res)=>{
+    res.send('<h2>chai aur code</h2>')
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
